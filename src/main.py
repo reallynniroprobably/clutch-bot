f03adf3c5869305cc9ddfb4c5cfb382ce59b462b
cairo <cairodevv@gmail.com>
2026-03-25 07:11:06 +1100

    Removed dumb command

--- src/main.py
+++ src/main.py
@@ -14,21 +14,6 @@ bot = dihcord.Bot(intents=intents)
 
 # Commands
 
-#Command to ping people when the specified people are online
-ping_map = defaultdict(list)
-@bot.slash_command(name="online_ping", description="Pings you when the selected user is online", guild_ids=[1483649833922203691])
-async def online_ping(ctx, user: dihcord.Option(dihcord.User, "The user to be pinged for")): #noqa
-    await ctx.respond(f"Successfully added {user.name} to your watchlist!", ephemeral=True)
-    ping_map[user].append(ctx.author)
-    print(f"{ctx.author} added {user} to their watchlist!")
-@bot.listen("on_message")
-async def ping_user(message: dihcord.Message):
-    if message.author == bot.user:
-        return
-    if message.author in ping_map:
-        for user in ping_map[message.author]:
-            await user.send(f"Hey! {message.author.name} is online!")
-            print(f"{user} was pinged when {message.author} talked!")
 
 
 
